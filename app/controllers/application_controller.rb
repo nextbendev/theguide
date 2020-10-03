a52aa15676395daa9fea3efb46cd13c6f12f73df
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-10-03 16:08:50 -0400

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def index
+    @categories = Category.all
+
+    cate = params[:cate]
+
+    if !cate.nil?
+      @blogs = Blog.where(:category_id => cate)
+    else
+    @blogs = Blog.all
+    end
+
+
+  end
 end
