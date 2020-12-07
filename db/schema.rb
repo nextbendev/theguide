9c44d4b4ca782fb5c6c8322bbbb01086e7bf39c8
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-12-07 07:24:09 -0500

    user add, db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_190708) do
+ActiveRecord::Schema.define(version: 2020_12_07_121901) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "headline", null: false
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 2020_10_01_190708) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "last_sign_in_at"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
